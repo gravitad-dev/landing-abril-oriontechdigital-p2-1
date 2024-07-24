52c72df6446681f8865eab8485456aae96614b8a
silvak <silvak.jeg@gmail.com>
2024-07-24 15:27:47 -0400

    changes menu texts

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,14 +1,14 @@
-import data from '../data.json';
-import { Link } from 'react-scroll';
-import PropTypes from 'prop-types';
-import SocialButtons from '../components/Socials';
+import data from "../data.json";
+import { Link } from "react-scroll";
+import PropTypes from "prop-types";
+import SocialButtons from "../components/Socials";
 
 function Footer() {
   return (
-    <footer className='flex w-full pt-10 pb-10 bg-blue-custom overflow-hidden'>
-      <div className='flex flex-col justify-center text-white mx-auto'>
-        <div className='p-12 flex items-center justify-center flex-wrap gap-4 sm:gap-8 xl:gap-12 2xl:gap-20 mx-auto'>
-          {data['section-footer'].links.map((link, index) => (
+    <footer className="flex w-full pt-10 pb-10 bg-blue-custom overflow-hidden">
+      <div className="flex flex-col justify-center text-white mx-auto">
+        <div className="p-12 flex items-center justify-center flex-wrap gap-4 sm:gap-8 xl:gap-12 2xl:gap-20 mx-auto">
+          {data["section-footer"].links.map((link, index) => (
             <Link
               key={index}
               to={link.href}
@@ -16,56 +16,46 @@ function Footer() {
               smooth={true}
               duration={500}
               offset={-85}
-              className='cursor-pointer'
+              className="cursor-pointer"
             >
-              <p className='mb-2 cursor-pointer hover:text-red-custom shadow-only'>
+              <p className="mb-2 cursor-pointer hover:text-red-custom shadow-only">
                 {link.title}
               </p>
             </Link>
           ))}
-          <a href={data['section-header']['blog-ref']} target='_blank'>
-            <p className='mb-2 cursor-pointer hover:text-red-custom'>
-              {data['section-header'].blog}
+          <a href={data["section-header"]["blog-ref"]} target="_blank">
+            <p className="mb-2 cursor-pointer hover:text-red-custom">
+              {data["section-header"].blog}
             </p>
           </a>
-          <a href='https://blog.oriontechdigital.com/contacto/' target='_blank'>
-            <p className='mb-2 cursor-pointer hover:text-red-custom'>
-              {data['section-header']['contact-button']}
+          <a href="https://blog.oriontechdigital.com/contacto/" target="_blank">
+            <p className="mb-2 cursor-pointer hover:text-red-custom">
+              {data["section-header"]["contact-button"]}
             </p>
           </a>
         </div>
         <div>
           <SocialButtons />
         </div>
-        <div className='w-screen flex items-center flex-wrap-reverse justify-around gap-6 sm:gap-12 p-4'>
-          <div className='flex justify-center items-center gap-4 flex-wrap px-2'>
+        <div className="w-screen flex items-center flex-wrap-reverse justify-around gap-6 sm:gap-12 p-4">
+          <div className="flex justify-center items-center gap-4 flex-wrap px-2">
             <Link
-              to={'home'}
+              to={"home"}
               spy={false}
               smooth={true}
               duration={500}
               offset={-85}
-              className='cursor-pointer'
+              className="cursor-pointer"
             >
               <img
-                src={data['section-footer']['logo-white'].url}
-                alt={data['section-footer']['logo-white'].alt}
-                className='h-6 sm:h-6 mini:w-50 lg:w-10 lg:h-auto hover:invert-[0.1]'
+                src={data["section-footer"]["logo-white"].url}
+                alt={data["section-footer"]["logo-white"].alt}
+                className="h-6 sm:h-6 mini:w-50 lg:w-10 lg:h-auto hover:invert-[0.1]"
               />
             </Link>
-            <p className='hidden mini:block text-[10px] truncate'>
-              {data['section-footer'].copywrite}
+            <p className="hidden mini:block text-[10px] truncate">
+              {data["section-footer"].copywrite}
             </p>
-            <a href={data['section-footer'].privacy.url}>
-              <p className='text-[8px] xxs:text-[10px] truncate hover:text-light-blue-custom'>
-                {data['section-footer'].privacy.text}
-              </p>
-            </a>
-            <a href={data['section-footer'].terms.url}>
-              <p className='text-[8px] xxs:text-[10px] truncate hover:text-light-blue-custom'>
-                {data['section-footer'].terms.text}
-              </p>
-            </a>
           </div>
         </div>
       </div>
@@ -76,8 +66,8 @@ function Footer() {
 function RenderLink({ link }) {
   if (link.external === true) {
     return (
-      <a href={link.url} target='_blank' rel='noopener noreferrer'>
-        <p className='text-[14px] hover:text-light-blue-custom cursor-pointer'>
+      <a href={link.url} target="_blank" rel="noopener noreferrer">
+        <p className="text-[14px] hover:text-light-blue-custom cursor-pointer">
           {link.text}
         </p>
       </a>
@@ -90,9 +80,9 @@ function RenderLink({ link }) {
         smooth={true}
         duration={500}
         offset={-75}
-        className='cursor-pointer'
+        className="cursor-pointer"
       >
-        <p className='text-[14px]  hover:text-light-blue-custom cursor-pointer'>
+        <p className="text-[14px]  hover:text-light-blue-custom cursor-pointer">
           {link.text}
         </p>
       </Link>
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,43 +1,43 @@
-import { FaInstagram } from 'react-icons/fa';
-import { FaFacebook } from 'react-icons/fa';
-import { FaLinkedin } from 'react-icons/fa';
-import { FaXTwitter } from 'react-icons/fa6';
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const social = [
   {
     id: 0,
-    url: 'https://www.instagram.com/oriontechdigital/',
+    url: "https://www.instagram.com/oriontechdigital/",
     icon: <FaInstagram />,
   },
   {
     id: 1,
-    url: 'https://www.facebook.com/profile.php?id=61560827337379',
+    url: "https://www.facebook.com/profile.php?id=61560827337379",
     icon: <FaFacebook />,
   },
   {
     id: 2,
-    url: 'https://www.linkedin.com/company/103587270/admin/dashboard/',
+    url: "https://www.linkedin.com/company/103587270/admin/dashboard/",
     icon: <FaLinkedin />,
   },
   {
     id: 3,
-    url: 'https://x.com/Oriontechdig',
+    url: "https://x.com/Oriontechdig",
     icon: <FaXTwitter />,
   },
 ];
 
 function SocialButtons() {
   return (
-    <div className='flex py-10 justify-center'>
+    <div className="flex py-10 justify-center">
       <ul>
-        <li className='flex gap-[60px]'>
+        <li className="flex gap-4">
           {social.map((item) => (
             <a
               key={item.id}
               href={item.url}
-              target='_blank'
-              rel='noreferrer'
-              className=' bg-white h-[28px] w-[28px] rounded-[4px] text-black text-[16px] flex justify-center items-center'
+              target="_blank"
+              rel="noreferrer"
+              className=" text-white h-[28px] w-[28px] rounded-[4px] text-[16px] flex justify-center items-center"
             >
               {item.icon}
             </a>
